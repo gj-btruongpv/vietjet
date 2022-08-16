01417bf8804ce89e39f8122997e369cdebd49fe2
Truong Pham <truong.pham@bstarsolutions.com>
2022-08-16 22:22:41 +0700

    log base url
    log base url

--- lib/vietjet_router.dart
+++ lib/vietjet_router.dart
@@ -5,10 +5,10 @@ import 'package:vietjet_sdk/globals.dart' as globals;
 class AppVietJetRouter {
   static GlobalKey<NavigatorState>? navigatorKey = null;
 
-  static Map<String, WidgetBuilder> registerRoutesVietJet(
-      {required GlobalKey<NavigatorState> key, required String baseUrl}) {
+  static Map<String, WidgetBuilder> registerRoutesVietJet({required GlobalKey<NavigatorState> key, required String baseUrl}) {
     navigatorKey = key;
     globals.baseUrl = baseUrl;
+    debugPrint('VJ Base URL ${globals.baseUrl}');
     return {
       LoginScreen.routePath: (context) => LoginScreen.registerRoute(context),
       PaymentScreen.routePath: (context) => const PaymentScreen(),
