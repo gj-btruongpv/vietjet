f1471099cd38f2bd0fb8d6e79792abae14556cd2
Truong Pham <truong.pham@bstarsolutions.com>
2022-08-16 13:13:53 +0700

    create model user
    create model user

--- vietjet_example/lib/app_router.dart
+++ vietjet_example/lib/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vietjet/vietjet.dart';
 
-
 class AppRouter {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   static Map<String, WidgetBuilder> registerRoutes() {
