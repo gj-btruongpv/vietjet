a5889692468cb5a8e56d4fbfd2d0ee7a78b5c00f
Truong Pham <truong.pham@bstarsolutions.com>
2022-08-19 15:14:16 +0700

    fix export
    fix export

--- lib/vietjet.dart
+++ lib/vietjet.dart
@@ -1,4 +1,4 @@
 export 'features/vietjet_screen.dart';
 export 'models/user_model.dart';
 export 'package:vietjet_sdk/vietjet_router.dart';
-import 'package:payment_sdk/payment.dart';
+export 'package:payment_sdk/payment.dart';
